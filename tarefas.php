993380186344aacd1402c6db4621c893a05aeed2
xlbruce <gilson@Gilsons-MacBook-Pro.local>
2016-02-16 15:11:03 -0200

    Código refatorado

--- tarefas.php
+++ tarefas.php
@@ -2,7 +2,9 @@
 require_once "functions.php";
 
 $ERROR_MESSAGES = [
-    "miss_param" => 'O parâmetro "%s" é obrigatório'
+    "miss_param" => 'O parâmetro "%s" é obrigatório',
+    "task_not_found" => 'A tarefa %s não foi encontrada',
+    "task_already_exists" => 'Já existe uma tarefa com o nome "%s"'
 ];
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -12,9 +14,18 @@ if ($method == 'POST') { //POST é somente usado para a inserção de tarefas
     if (isset($_POST['nome']) && (!empty($_POST['nome']))) {
         $name = $_POST['nome'];
         $description = (isset($_POST['descricao'])) ? $_POST['descricao'] : "";
-        $id = addTask($name, $description);
-        echo json_encode($id, JSON_PRETTY_PRINT);
-        return http_response_code($RESPONSE_CODES['ok']);
+        $task = addTask($name, $description);
+        if($task) {
+            echo json_encode($task, JSON_PRETTY_PRINT);
+            return http_response_code($RESPONSE_CODES['ok']);
+        } else {
+            $responseCode = $RESPONSE_CODES['bad_request'];
+            echo json_encode([
+                "status" => $responseCode,
+                "mensagem" => sprintf($ERROR_MESSAGES['task_already_exists'], $name)
+            ]);
+            return http_response_code($responseCode);
+        }
     } else {
         $responseCode = $RESPONSE_CODES['bad_request'];
         echo json_encode([
@@ -44,11 +55,19 @@ if ($method == 'POST') { //POST é somente usado para a inserção de tarefas
             return http_response_code($responseCode);
         }
     } elseif (!empty(($id = $vars['id']))) { //Marcar conclusão de uma tarefa
+
         $success = taskDone($id);
         if ($success) {
             $success->status = 200;
             echo json_encode($success, JSON_PRETTY_PRINT);
             return http_response_code($RESPONSE_CODES['ok']);
+        } else {
+            $responseCode = http_response_code(403);
+            echo json_encode([
+                "status" => $responseCode,
+                "mensagem" => sprintf($ERROR_MESSAGES['task_not_found'], $id)
+            ]);
+            return $responseCode;
         }
     }
 }
