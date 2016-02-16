0c77ef26fe5b9015b69d6caeb9f820c5fbdeedf1
xlbruce <gilson@Gilsons-MacBook-Pro.local>
2016-02-16 16:37:00 -0200

    Código refatorado

--- functions.php
+++ functions.php
@@ -1,11 +1,6 @@
 <?php
 date_default_timezone_set("America/Sao_Paulo");
 
-$RESPONSE_CODES = [
-                   "ok" => 200,
-                   "no_content" => 204,
-                   "bad_request" => 400
-                  ];
 $TASKS_DIR = 'tasks';
 $TASKS = opendir($TASKS_DIR);
 
@@ -15,7 +10,6 @@ function getAllTasks()
     global $TASKS;
     global $TASKS_DIR;
 
-    $response = [];
     while (($entry = readdir($TASKS))) {
         if ($entry != "." && $entry != "..") { //Ignora o diretório atual e o anterior (um nivel acima)
             $response[] = json_decode(file_get_contents("$TASKS_DIR/$entry"));
@@ -25,30 +19,41 @@ function getAllTasks()
     return json_encode($response, JSON_PRETTY_PRINT);
 }
 
+function getTask($id) {
+    global $TASKS_DIR;
+
+    $content = file_get_contents("$TASKS_DIR/$id.json");
+    if($content) {
+        return json_decode($content);
+    }
+    return false;
+}
+
 //Adiciona uma tarefa e retorna o objeto adicionado em caso de sucesso
 function addTask($name, $description = "")
 {
     global $TASKS_DIR;
 
-
-    $id = generateId();
-    $date = date("Y-m-d");
-    $task = new stdClass();
-    $task->id = $id;
-    $task->nome = $name;
-    $task->descricao = $description;
-    $task->concluida = false;
-    $task->criacao = $date;
-    $task->modificada = $date;
-
-    try {
-        writeJson("$TASKS_DIR/$id.json", $task);
-    } catch (Exception $e) {
-        echo "Erro ao serializar";
-        echo $e;
-        return false;
+    if (!findTask($name)) {
+        $id = generateId();
+        $date = date("Y-m-d");
+        $task = new stdClass();
+        $task->id = $id;
+        $task->nome = $name;
+        $task->descricao = $description;
+        $task->concluida = false;
+        $task->criacao = $date;
+        $task->modificada = $date;
+        try {
+            writeJson("$TASKS_DIR/$id.json", $task);
+        } catch (Exception $e) {
+            echo "Erro ao serializar";
+            echo $e;
+            return false;
+        }
+        return $task;
     }
-    return $task;
+    return false;
 }
 
 //Salva um arquivo em disco no formato JSON
@@ -62,24 +67,34 @@ function writeJson($filename, $obj)
 
 function removeTask($id)
 {
-    global $RESPONSE_CODES;
     global $TASKS_DIR;
 
     unlink("$TASKS_DIR/$id.json");
-    http_response_code($RESPONSE_CODES['no_content']); //No content
     return true;
 }
 
-function editTask($taskName, $description = "") {
+function editTask($taskName, $description = "")
+{
+    global $TASKS_DIR;
+
+    $task = findTask($taskName);
+    if ($task) {
+        $task->descricao = $description;
+        $task->modificada = date("Y-m-d");
+        $filename = $TASKS_DIR . '/' . $task->id . '.json';
+        writeJson($filename, $task);
+        return $task;
+    }
+    return false;
+}
+
+function findTask($taskName)
+{
     global $TASKS_DIR;
 
     $tasks = json_decode(getAllTasks());
-    foreach($tasks as $k => $v) {
-        if($tasks[$k]->nome == $taskName) {
-            $tasks[$k]->descricao = $description;
-            $tasks[$k]->modificada = date("Y-m-d");
-            $filename = $TASKS_DIR . '/' . $tasks[$k]->id . '.json';
-            writeJson($filename, $tasks[$k]);
+    foreach ($tasks as $k => $v) {
+        if ($tasks[$k]->nome == $taskName) {
             return $tasks[$k];
         }
     }
@@ -96,9 +111,8 @@ function taskDone($id)
     global $TASKS_DIR;
 
     $filename = "$TASKS_DIR/$id.json";
-    $content = file_get_contents($filename);
-    if ($content) {
-        $task = json_decode($content);
+    $task = getTask($id);
+    if($task) {
         $task->concluida = true;
         $task->modificada = date("Y-m-d");
         writeJson($filename, $task);
